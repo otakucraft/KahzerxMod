2085e2eea93c24fda085f77778dd4b7073dd5f75
kahzerx <kahzerx@tutanota.com>
2021-10-01 00:24:52 +0200

    member team extension done

--- src/main/java/com/kahzerx/kahzerxmod/extensions/memberExtension/MemberExtension.java
+++ src/main/java/com/kahzerx/kahzerxmod/extensions/memberExtension/MemberExtension.java
@@ -4,35 +4,16 @@ import com.kahzerx.kahzerxmod.Extensions;
 import com.kahzerx.kahzerxmod.extensions.ExtensionSettings;
 import com.kahzerx.kahzerxmod.extensions.GenericExtension;
 import net.minecraft.scoreboard.Team;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Formatting;
 
-import java.sql.*;
-import java.util.Objects;
+import java.util.Collection;
 
 public class MemberExtension extends GenericExtension implements Extensions {
-    private Connection conn;
-
     public MemberExtension(ExtensionSettings settings) {
         super(settings);
-    }
-
-    @Override
-    public void onCreateDatabase(Connection conn) {
-        if (!this.getSettings().isEnabled()) {
-            return;
-        }
-        this.conn = conn;
-        try {
-            String createBackDatabase = "CREATE TABLE IF NOT EXISTS `ever_joined` (" +
-                    "`uuid` VARCHAR(50) PRIMARY KEY NOT NULL," +
-                    "`joined` NUMERIC(1) DEFAULT NULL," +
-                    "FOREIGN KEY(uuid) REFERENCES player(uuid));";
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate(createBackDatabase);
-            stmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -40,32 +21,21 @@ public class MemberExtension extends GenericExtension implements Extensions {
         if (!this.getSettings().isEnabled()) {
             return;
         }
-        try {
-            boolean isNew = false;
-            String query = "SELECT joined FROM ever_joined WHERE uuid = ?";
-            PreparedStatement ps = this.conn.prepareStatement(query);
-            ps.setString(1, player.getUuidAsString());
-            ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                String q = "INSERT INTO ever_joined (uuid, joined) VALUES (?, ?)";
-                PreparedStatement p = this.conn.prepareStatement(q);
-                p.setString(1, player.getUuidAsString());
-                p.setInt(2, 1);
-                p.executeUpdate();
-                p.close();
-                isNew = true;
+        MinecraftServer server = player.getServer();
+        assert server != null;
+        Collection<String> teamNames = server.getScoreboard().getTeamNames();
+        if (!teamNames.contains("MIEMBRO")) {
+            Team team = server.getScoreboard().addTeam("MIEMBRO");
+            team.setPrefix(new LiteralText("[MIEMBRO] ").styled(style -> style.withBold(true).withColor(Formatting.GREEN)));
+        }
+        if (teamNames.contains("MIEMBRO")) {
+            Team playerTeam = server.getScoreboard().getPlayerTeam(player.getName().getString());
+            System.out.println(playerTeam);
+            if (playerTeam != null) {
+                return;
             }
-            rs.close();
-            ps.close();
-            if (isNew) {
-                for (Team t : Objects.requireNonNull(player.getServer()).getScoreboard().getTeams()) {
-                    if (t.getName().equals("MIEMBRO")) {
-                        player.getServer().getScoreboard().addPlayerToTeam(player.getName().getString(), t);
-                    }
-                }
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            Team team = server.getScoreboard().getTeam("MIEMBRO");
+            server.getScoreboard().addPlayerToTeam(player.getName().getString(), team);
         }
     }
 
