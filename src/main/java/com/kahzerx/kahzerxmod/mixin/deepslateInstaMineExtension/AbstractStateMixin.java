26a9a69b99943d2e3c1a4cd10851b0b0b14d5b77
kahzerx <kahzerx@tutanota.com>
2021-12-06 01:14:33 +0100

    deepslate instamine

--- src/main/java/com/kahzerx/kahzerxmod/mixin/deepslateInstaMineExtension/AbstractStateMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/deepslateInstaMineExtension/AbstractStateMixin.java
@@ -1,6 +1,7 @@
 package com.kahzerx.kahzerxmod.mixin.deepslateInstaMineExtension;
 
 import com.kahzerx.kahzerxmod.extensions.deepslateInstaMineExtension.DeepslateInstaMineExtension;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
@@ -11,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = {"net.minecraft.block.AbstractBlock$AbstractBlockState"})
+@Mixin(AbstractBlock.AbstractBlockState.class)
 public abstract class AbstractStateMixin {
     @Shadow public abstract Block getBlock();
     @Inject(method = "getHardness", at = @At("HEAD"), cancellable = true)
-    private void onGetHardness(BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir) {
+    public void onGetHardness(BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir) {
         if (DeepslateInstaMineExtension.isExtensionEnabled && this.getBlock() == Blocks.DEEPSLATE) {
             cir.setReturnValue(1.5F);
         }
