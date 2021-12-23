c839daef28ce695d572210d419bf89e9cdf25787
kahzerx <kahzerx@tutanota.com>
2021-12-23 15:50:12 +0100

    no server shadow

--- src/main/java/com/kahzerx/kahzerxmod/mixin/pranksExtension/PlayerMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/pranksExtension/PlayerMixin.java
@@ -4,16 +4,13 @@ import com.kahzerx.kahzerxmod.extensions.prankExtension.PrankExtension;
 import com.kahzerx.kahzerxmod.extensions.prankExtension.PrankLevel;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,8 +18,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class PlayerMixin extends PlayerEntity {
-    @Shadow @Final public MinecraftServer server;
-
     public PlayerMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
         super(world, pos, yaw, profile);
     }
