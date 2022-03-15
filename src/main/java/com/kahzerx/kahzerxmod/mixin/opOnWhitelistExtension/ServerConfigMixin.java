1830b9db67c0d5b741ab30b8277caa3e817a77c1
kahzerx <kahzerx@tutanota.com>
2022-03-15 22:25:07 +0100

    debug op on whitelist

--- src/main/java/com/kahzerx/kahzerxmod/mixin/opOnWhitelistExtension/ServerConfigMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/opOnWhitelistExtension/ServerConfigMixin.java
@@ -6,6 +6,8 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.server.ServerConfigEntry;
 import net.minecraft.server.ServerConfigList;
 import net.minecraft.server.WhitelistEntry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,12 +20,16 @@ import java.util.UUID;
 
 @Mixin(ServerConfigList.class)
 public class ServerConfigMixin<K, V extends ServerConfigEntry<K>> {
+    private static final Logger LOGGER = LogManager.getLogger();
     @Inject(method = "add", at = @At("RETURN"))
     public void onWhitelistAdd(V entry, CallbackInfo ci) {
         if (OpOnWhitelistExtension.isExtensionEnabled && OpOnWhitelistExtension.server != null && entry instanceof WhitelistEntry) {
             try {
+                LOGGER.debug(entry);
                 JSONParser parser = new JSONParser();
                 JSONObject obj = (JSONObject) parser.parse(new Gson().toJson(entry));
+                LOGGER.debug(obj);
+                LOGGER.debug(obj.get("key"));
                 Optional<GameProfile> profile = OpOnWhitelistExtension.server.getUserCache().getByUuid(UUID.fromString((String) ((JSONObject) obj.get("key")).get("id")));
                 if (profile.isEmpty()) {
                     return;
