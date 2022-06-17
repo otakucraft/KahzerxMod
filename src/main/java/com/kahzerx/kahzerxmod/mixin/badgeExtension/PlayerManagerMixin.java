bcd31f626a64b24668d4de51db74d32690b0be99
kahzerx <kahzerx@tutanota.com>
2022-06-17 12:48:45 +0200

    badge extension fix

--- src/main/java/com/kahzerx/kahzerxmod/mixin/badgeExtension/PlayerManagerMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/badgeExtension/PlayerManagerMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.HoverEvent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -18,6 +19,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.List;
+
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
     @Shadow @Final private MinecraftServer server;
@@ -26,18 +29,33 @@ public class PlayerManagerMixin {
     private void onBroadcast(ServerPlayerEntity instance, SignedMessage message, MessageSender sender, RegistryKey<MessageType> typeKey) {
         ServerPlayerEntity player = server.getPlayerManager().getPlayer(sender.uuid());
         if (BadgeExtension.isExtensionEnabled && player != null) {
-            MutableText t = Text.literal(String.format("Name: %s", player.getName().getString()));
-            if (!BadgeExtension.playerBadges.get(sender.uuid().toString()).isEmpty()) {
-                t.append(Text.literal("\nBadges:"));
+            MutableText t = Text.literal("");
+            List<BadgeInstance> badges = BadgeExtension.playerBadges.get(sender.uuid().toString());
+            if (badges.size() != 0) {
+                MutableText hover = Text.literal("");
+                hover.append(Text.literal("Badges:").styled(style -> style.withColor(Formatting.WHITE)));
+                for (BadgeInstance badgeInstance : badges) {
+                    hover.append(Text.literal("\n" + badgeInstance.getBadge()).styled(style -> style.withColor(Formatting.byColorIndex(badgeInstance.getColorIndex()))));
+                    hover.append(Text.literal(String.format(": %s", badgeInstance.getDescription())).styled(style -> style.withColor(Formatting.WHITE)));
+                }
+                MutableText bgs = Text.literal("");
+                for (int i = badges.size() - 1; i >= 0; i--) {
+                    BadgeInstance badge = badges.get(i);
+                    bgs.append(Text.literal(badge.getBadge()).styled(style -> style.withColor(Formatting.byColorIndex(badge.getColorIndex()))));
+                    bgs.append(Text.literal("").styled(style -> style.withColor(Formatting.WHITE)));
+                    if (i < badges.size() - 2 || i == 0) {
+                        break;
+                    }
+                    bgs.append(Text.literal(" "));
+                }
+                t.append(bgs.styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hover))));
+                t.append(Text.literal(" "));
             }
-            for (BadgeInstance badgeInstance : BadgeExtension.playerBadges.get(sender.toString())) {
-                t.append(Text.literal("\n" + badgeInstance.getBadge()).styled(style -> style.withColor(Formatting.byColorIndex(badgeInstance.getColorIndex()))));
-                t.append(Text.literal(String.format(": %s", badgeInstance.getDescription())).styled(style -> style.withColor(Formatting.WHITE)));
-            }
-            // TODO clavarle el mensaje este.
-//            ((TranslatableTextContent) message)
-//            ((LiteralTextContent) ((TranslatableTextContent) message).getArgs()[0]).styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, t)));
+            t.append(message.signedContent());
+            SignedMessage sm = new SignedMessage(t, message.signature(), message.unsignedContent());
+            instance.sendChatMessage(sm, sender, typeKey);
+        } else {
+            instance.sendChatMessage(message, sender, typeKey);
         }
-        instance.sendChatMessage(message, sender, typeKey);
     }
 }
